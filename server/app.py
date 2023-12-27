cd34a84fd86c4ae564254e76a676c9de4fba4944
+ <+>
2023-12-27 12:03:31 +0300

    completed

--- server/app.py
+++ server/app.py
@@ -17,10 +17,31 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plant_dict_list=[plant.to_dict() for plant in Plant.query.all()]
+        response=make_response(plant_dict_list,200)
+        return response
+    def post(self):
+        data=request.get_json()
+        new_plant=Plant(
+            name=data["name"],
+            image=data["image"],
+            price=data["price"],           
+        )
+        db.session.add(new_plant)
+        db.session.commit()
+        response=make_response(new_plant.to_dict(),201)
+        return response
+api.add_resource(Plants,"/plants")
 
 class PlantByID(Resource):
-    pass
+     def get(self,id):
+         single_plant=Plant.query.filter_by(id=id).first()
+         response=make_response(single_plant.to_dict(),200)
+         return response
+api.add_resource(PlantByID,"/plants/<int:id>")
+
+
         
 
 if __name__ == '__main__':
